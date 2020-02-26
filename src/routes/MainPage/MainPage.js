68bb73e5ffc493a17980d79a7b2f6adf5ac98e92
SirPotatoIV <otoolej33@gmail.com>
2020-02-26 15:44:54 -0600

    Forgot to add changes to Main Page on the last commit. This commit includes the addition of section tags.

--- src/routes/MainPage/MainPage.js
+++ src/routes/MainPage/MainPage.js
@@ -7,20 +7,24 @@ import { projects } from "../../data/projects";
 export default function MainPage() {
   return (
     <div className="main-page">
-      <h1>Projects</h1>
-      {projects.map(project => (
-        <ProjectCard
-          deployedLink={project.deployedLink}
-          description={project.description}
-          image={project.image}
-          key={project.key}
-          name={project.name}
-          repositoryLink={project.repositoryLink}
-          techUsed={project.techUsed}
-        />
-      ))}
-      <h1>Bio</h1>
-      <Bio />
+      <section>
+        <h1>Projects</h1>
+        {projects.map(project => (
+          <ProjectCard
+            deployedLink={project.deployedLink}
+            description={project.description}
+            image={project.image}
+            key={project.key}
+            name={project.name}
+            repositoryLink={project.repositoryLink}
+            techUsed={project.techUsed}
+          />
+        ))}
+      </section>
+      <section>
+        <h1>Bio</h1>
+        <Bio />
+      </section>
     </div>
   );
 }
