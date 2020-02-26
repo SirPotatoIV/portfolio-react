8dee257dec1b597f454e6ee7ccfdbde43fa65151
SirPotatoIV <otoolej33@gmail.com>
2020-02-26 10:19:18 -0600

    Created a navbar component.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NavBar(){
+    return(
+        <div
+            className="NavBar"
+        >
+            <p>The NavBar</p>
+        </div>
+    )
+}
