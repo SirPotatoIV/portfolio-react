6da6985de066a5c458d632f5a4e2d6495424b955
SirPotatoIV <otoolej33@gmail.com>
2020-02-26 10:46:57 -0600

    Created a new component called logo. Added some jsx and styling just for testing purposes.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
-import NavBarLinks from "../NavBarLinks";
 import "./NavBar.css";
+import Logo from "../Logo/";
+import NavBarLinks from "../NavBarLinks";
 
 export default function NavBar() {
   return (
     <div className="navbar">
-      <p>The NavBar</p>
-      <NavBarLinks></NavBarLinks>
+      <Logo />
+      <NavBarLinks />
     </div>
   );
 }
